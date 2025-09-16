9810363b458b4820277e452420edbe20b8b66f42
RicardoMomberg <65039145+RicardoMomberg@users.noreply.github.com>
2025-09-16 11:32:24 -0300

    01 - Responsive Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,25 +23,45 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text('Image Gallery 2')),
-      body: Center(
-        child: screenSize.width < 600
-            ? Column(
-                children: [
-                  Text('Small screen layout'),
-                  Image.network(
-                    'https://www.combatarena.net/cdn/shop/articles/bjj.jpg?v=1695911535',
-                  ),
-                ],
-              )
-            : Column(
-                children: [
-                  Text('Large screen layout'),
-                  Image.network(
-                    'https://cdn.evolve-mma.com/wp-content/uploads/2015/05/why-bjj-is-the-perfect-martial-art.jpg',
-                  ),
-                ],
-              ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 600) {
+            return ListView(children: buildImageList());
+          } else {
+            return GridView.count(
+              crossAxisCount: 3,
+              children: buildImageList(),
+            );
+          }
+        },
       ),
+      // Center(
+      //   child: screenSize.height < 600
+      //       ? Column(
+      //           children: [
+      //             Text('Small screen layout'),
+      //             Image.network(
+      //               'https://www.combatarena.net/cdn/shop/articles/bjj.jpg?v=1695911535',
+      //             ),
+      //           ],
+      //         )
+      //       : Column(
+      //           children: [
+      //             Text('Large screen layout'),
+      //             Image.network(
+      //               'https://cdn.evolve-mma.com/wp-content/uploads/2015/05/why-bjj-is-the-perfect-martial-art.jpg',
+      //             ),
+      //           ],
+      //         ),
+      // ),
     );
+  }
+
+  List<Widget> buildImageList() {
+    return List.generate(10, (index) {
+      return Image.network(
+        'https://cdn.evolve-mma.com/wp-content/uploads/2015/05/why-bjj-is-the-perfect-martial-art.jpg',
+      );
+    });
   }
 }
